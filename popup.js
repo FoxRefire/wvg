862b7c0d006ef0e9d878f194fe3925ce406d6e32
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-04-10 00:04:17 +0900

    Refactor guessing attempt

--- popup.js
+++ popup.js
@@ -52,22 +52,83 @@ function selectRequest(){
     document.getElementById('selectRequest').style.display='block';
 }
 
-async function guess(){
-    endpoint = document.getElementById('guessr').value;
-    payload = {
-        "PSSH": psshs[userInputs['pssh']],
-        "Headers": requests[userInputs['license']]['headers'],
-        "LicenseUrl": requests[userInputs['license']]['url']
-    }
-    console.log(JSON.stringify(payload));
-    console.log(endpoint);
+var CommonWV = async function(pssh, licUrl) {
+    const serverAddr = "http://127.0.0.1:18888";
+    console.group("fetch cert...");
+    let certBuffer = await fetch(licUrl, {
+        body: new Uint8Array([0x08, 0x04]),
+        headers: {
+            "Content-Type": "application/octet-stream"
+        },
+        method: "POST"
+    }).then(resp => resp.arrayBuffer());
+    let certB64 = btoa(String.fromCharCode(...new Uint8Array(certBuffer)));
+    console.log(certB64);
+    console.groupEnd();
 
-    let json = await fetch(endpoint, {
-        body: JSON.stringify(payload),
-        headers: {"Content-Type": "application/json"},
+    console.group("fetch challenge...");
+    let jsonC = await fetch(serverAddr + "/getchallenge", {
+        body: JSON.stringify({
+            "PSSH": pssh,
+            "CertBase64": certB64
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "POST"
     }).then(resp => resp.json());
-    document.getElementById('result').value=JSON.stringify(json.keys, null , "\t");
+    let challengeBase64 = jsonC.challengeBase64;
+    console.log(challengeBase64);
+    console.groupEnd();
+
+    console.group("fetch license...");
+    let licBuffer = await fetch(licUrl, {
+        body: Uint8Array.from(atob(challengeBase64), (c) => c.charCodeAt(0)),
+        headers: {
+            "Content-Type": "application/octet-stream"
+        },
+        method: "POST"
+    }).then(resp => resp.arrayBuffer());
+    let licB64 = btoa(String.fromCharCode(...new Uint8Array(licBuffer)));
+    console.log(licB64);
+    console.groupEnd();
+
+    console.group("get keys...");
+    let jsonK = await fetch(serverAddr + "/getkeys", {
+        body: JSON.stringify({
+            "PSSH": pssh,
+            "ChallengeBase64": challengeBase64,
+            "LicenseBase64": licB64
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST"
+    }).then(resp => resp.json());
+    let keys = jsonK.keys;
+    console.log(keys);
+    console.groupEnd();
+    return keys;
+};
+
+async function guess(){
+    endpoint = document.getElementById('guessr').value;
+    // payload = {
+    //     "PSSH": psshs[userInputs['pssh']],
+    //     "Headers": requests[userInputs['license']]['headers'],
+    //     "LicenseUrl": requests[userInputs['license']]['url']
+    // }
+    // console.log(JSON.stringify(payload));
+    // console.log(endpoint);
+    //
+    // let json = await fetch(endpoint, {
+    //     body: JSON.stringify(payload),
+    //     headers: {"Content-Type": "application/json"},
+    //     method: "POST"
+    // }).then(resp => resp.json());
+
+    const result=await CommonWV(psshs[userInputs['pssh']],requests[userInputs['license']]['url'])
+    document.getElementById('result').value=result;
 }
 
 if(psshs.length!=0){
