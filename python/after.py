9b64cd4c55503f1aab5503443220edf8521a172d
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-04-25 01:37:29 +0900

    Shows only keys

--- python/after.py
+++ python/after.py
@@ -4,7 +4,8 @@ cdm.parse_license(session_id, licence)
 # get keys
 keys=""
 for key in cdm.get_keys(session_id):
-    keys+=f"[{key.type}] {key.kid.hex}:{key.key.hex()}\n"
+    if key.type=="CONTENT":
+        keys+=f"{key.kid.hex}:{key.key.hex()}\n"
 
 # close session, disposes of session data
 cdm.close(session_id)
