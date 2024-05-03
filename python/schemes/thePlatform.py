835ccf5a45b15b4e59d2dc783d33ce5946e0beac
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-05-03 19:21:17 +0900

    thePlatform(rte.ie)

---
+++ python/schemes/thePlatform.py
@@ -0,0 +1,9 @@
+payload = json.loads(base64.b64decode(licBody.encode()).decode())
+b64challenge = base64.b64encode(challenge).decode()
+payload["getWidevineLicense"]["widevineChallenge"]=b64challenge
+res = await (await pyfetch(licUrl,
+    method="POST",
+    headers=licHeaders,
+    body=json.dumps(payload)
+)).json()
+licence = base64.b64decode(res["getWidevineLicenseResponse"]["license"].encode())
