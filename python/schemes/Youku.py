43a1869418feabba77474c48c0eba00d8b3ce27e
dreammu <mumugugugu@gmail.com>
2024-06-08 21:07:01 +0800

    fix youku
    solve https://github.com/FoxRefire/wvg/issues/14

--- python/schemes/Youku.py
+++ python/schemes/Youku.py
@@ -1,9 +1,11 @@
 import urllib.parse
 payload = urllib.parse.parse_qs(loadBody("str"))
 challengeB64 = base64.b64encode(challenge).decode()
-payload['licenseRequest'] = challengeB64
+# keep payload value as list
+payload['licenseRequest'] = [challengeB64]
+# convert payload to a dictionary with a single value
+payload = {k: v[0] for k, v in payload.items()}
 payload = urllib.parse.urlencode(payload)
 
 res = await corsFetch(licUrl, "POST", licHeaders, payload, "json")
 licence = res['data']
-
