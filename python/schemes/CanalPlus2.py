13743879acb51741c87a79cdbb85ac670a193a22
CDM-Project <CDM-Project@cdm-project.com>
2024-09-27 00:30:48 -0400

    Added CanalPlusPL / Removed arbitrary Canal+
    - popup.html: Removec Canal+ and Canal+(Alt) -- as far as I can tell widely untested
    
    - CanalPlus.py: Removed
    
    - CanalPlus2.py: Removed
    
    - CanalPlusPL.py: Added support for Canal+ Poland with service certificate
    
    - selectRules.conf: Adjusted for new CanalPlusPL schema

--- python/schemes/CanalPlus2.py
+++
@@ -1,5 +0,0 @@
-import xml.etree.ElementTree as ET
-b64challenge = base64.b64encode(challenge).decode()
-res = await corsFetch(licUrl, "POST", licHeaders, b64challenge, "str")
-
-licence = ET.XML(res)[0][0].text
