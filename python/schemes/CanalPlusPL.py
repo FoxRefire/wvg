a9fd72ef4af56ded229353ba38d257925ba14ef4
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-10-01 23:40:10 +0900

    Delete python/schemes/CanalPlusPL.py

--- python/schemes/CanalPlusPL.py
+++
@@ -1,4 +0,0 @@
-import xml.etree.ElementTree as ET
-challenge_with_cert = getChallenge('b64', Cdm.common_privacy_cert)
-licence = await corsFetch(licUrl, "POST", licHeaders, challenge_with_cert, "blob")
-licence = ET.fromstring(licence).find('.//{http://www.canal-plus.com/DRM/V1}license').text
