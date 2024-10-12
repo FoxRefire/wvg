ec927221a234a92db89dd799b216efa69d72f54d
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-10-13 07:35:53 +0900

    Refactor: Separate history page from main page

--- popup/history.js
+++ popup/history.js
@@ -1,24 +1,9 @@
 let psshs=chrome.extension.getBackgroundPage().psshs;
 function showHistory(){
-    chrome.storage.local.get(null, ((data) => {
+    chrome.storage.local.get(null, (data => {
         let tree=jsonview.renderJSON(JSON.stringify(data), document.getElementById('histDisp'));
         jsonview.toggleNode(tree);
     }));
-    document.getElementById('home').style.display='none';
-    document.getElementById('noEME').style.display='none';
-    document.getElementById('history').style.display='grid';
-    document.getElementById('toggleHistory').style.display='none';
-}
-
-function backHistory(){
-    document.getElementById('histDisp').innerHTML="";
-    document.getElementById('history').style.display='none';
-    document.getElementById('toggleHistory').style.display='grid';
-    if(psshs.length){
-        document.getElementById('home').style.display='grid';
-    } else {
-        document.getElementById('noEME').style.display='grid';
-    }
 }
 
 function saveHistory(){
@@ -41,7 +26,7 @@ function clearHistory(){
         document.getElementById('histDisp').innerHTML="";
     }
 }
-document.getElementById('historyButton').addEventListener("click", showHistory);
-document.getElementById('backHistory').addEventListener("click", backHistory);
+
 document.getElementById('saveHistory').addEventListener("click", saveHistory);
 document.getElementById('clearHistory').addEventListener("click", clearHistory);
+showHistory()
